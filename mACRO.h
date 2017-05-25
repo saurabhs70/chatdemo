3e0930a7c734e2044a418669b2d69dffbc931a55
Jasmeet Singh <dinnysingh@gmail.com>
2017-05-25 17:28:43 +0530

    mentor to mentor chat logic

---
+++ mACRO.h
@@ -0,0 +1,13 @@
+//
+//  mACRO.h
+//  Chatapp
+//
+//  Created by umenit on 5/25/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#ifndef mACRO_h
+#define mACRO_h
+
+
+#endif /* mACRO_h */
