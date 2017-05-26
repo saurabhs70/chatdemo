2b42ce629396931ffac1773b1897ffcee26bc75f
Jasmeet Singh <dinnysingh@gmail.com>
2017-05-26 16:33:38 +0530

    macro file added

---
+++ Chatapp/mACRO.h
@@ -0,0 +1,13 @@
+//
+//  mACRO.h
+//  Chatapp
+//
+//  Created by umenit on 5/25/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#ifndef mACRO_h
+#define mACRO_h
+
+#define APPDELEGATE  ((AppDelegate *) [[UIApplication sharedApplication] delegate])
+#endif /* mACRO_h */
